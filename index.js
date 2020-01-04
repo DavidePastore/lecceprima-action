4c5c3c49cbef8e6c1bb4b23677889bd007b8abb6
Davide Pastore <pasdavide@gmail.com>
2020-01-04 18:36:18 +0100

    Change the parameters

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const github = require('@actions/github');
 const Parser = require('rss-parser');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  // `db-directory` input defined in action metadata file
+  const dbDirectory = core.getInput('db-directory');
+  console.log(`The database directory is ${dbDirectory}`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
@@ -14,10 +14,18 @@ try {
 
   console.log('Reading from the RSS feed...');
   let parser = new Parser();
-  let feed = parser.parseURL('http://www.lecceprima.it/rss', (err, feed) => {
+  parser.parseURL('http://www.lecceprima.it/rss', (err, feed) => {
+    if (err) {
+      console.log(`Error while parsing the RSS: ${err}`);
+    }
+
     console.log(feed.title);
+    console.log(`Feed items are: ${feed.items.length}`);
+
     feed.items.forEach(item => {
-      console.log(item.title + ':' + item.link + ' at ' + item.pubDate);
+      console.log(`${item.title} : ${item.link} at ${item.pubDate} or ${item.isoDate}`);
+      let onlyDate = item.isoDate.substring(0, 10);
+      console.log(`Only date is: ${onlyDate}`);
     });
     console.log('No more RSS feed to read');
   });
