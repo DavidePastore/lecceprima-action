74a98a890f98134fe06b292ad93052826fb7de20
Davide Pastore <pasdavide@gmail.com>
2020-01-05 18:39:15 +0100

    Write data in a different db based on the date

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const Parser = require('rss-parser');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const fs = require('fs');
 
 try {
   // `db-directory` input defined in action metadata file
@@ -9,7 +12,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 
   console.log('Reading from the RSS feed...');
@@ -22,10 +25,36 @@ try {
     console.log(feed.title);
     console.log(`Feed items are: ${feed.items.length}`);
 
+    if (!fs.existsSync(dbDirectory)){
+        fs.mkdirSync(dbDirectory);
+    }
+
     feed.items.forEach(item => {
       console.log(`${item.title} : ${item.link} at ${item.pubDate} or ${item.isoDate}`);
       let onlyDate = item.isoDate.substring(0, 10);
       console.log(`Only date is: ${onlyDate}`);
+      let dbFile = `${dbDirectory}/${onlyDate}.json`;
+      const adapter = new FileSync(dbFile);
+      const db = low(adapter);
+      db.defaults({ items: [] })
+        .write();
+      let collection = db
+        .defaults({ items: [] })
+        .get('items');
+      
+      let foundItem = items
+        .find({ link: item.link })
+        .value();
+
+      // Check if the item already exists
+      if (!foundItem) {
+        // Insert a new item
+        collection
+          .insert(item)
+          .write();
+
+        // TODO Write it on Telegram
+      }
     });
     console.log('No more RSS feed to read');
   });
